052ca83d31d88229a1afa7aa223bb93bf6985565
reap777-afk <bartxboxpass@gmail.com>
2025-03-13 18:57:45 +0100

    Add files via upload

---
+++ src/views/Pusta.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pusta-page">
+    <h1>Podstrona Pusta</h1>
+    <p>To jest pusta podstrona.</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.pusta-page {
+  text-align: center;
+  padding: 20px;
+}
+</style>
